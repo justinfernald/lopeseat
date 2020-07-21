af0d8351757168e20c262abedca67a0029edf60b
Will Garrett <willgarrett610@gmail.com>
2020-07-21 11:56:22 -0600

    Address searching

---
+++ getBuildings.php
@@ -0,0 +1,19 @@
+<?php
+require('api.php');
+
+$db = new db();
+$stmt = $db->prepare("SELECT * FROM Buildings");
+$stmt->bind_param("s",$query);
+
+$buildings = array();
+
+$db->exec();
+$results = $db->get();
+
+while ($row = $results->fetch_object()) {
+    array_push($buildings, $row);
+}
+
+echo json_encode($buildings);
+
+?>
