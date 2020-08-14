ec5caca61c1251ef805e12f1249157c016e02a9e
justinfernald <jfernald@my.gcu.edu>
2020-08-13 17:54:59 -0700

    fix

--- getDelivererStats.php
+++ getDelivererStats.php
@@ -14,6 +14,7 @@ if ($user == null) {
 // amount earned:  SELECT SUM(`delivery_fee` + `tip`) FROM `Orders` WHERE `deliverer`=0
 // combined: SELECT COUNT(*) AS count ,AVG(rating) AS average_rating ,SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(`arrived`,`claimed`)))) AS average_delivery_time, SUM(`delivery_fee` + `tip`) AS amount_earned FROM `Orders` WHERE `deliverer`=0
 
+$db = new db();
 $stmt = $db->prepare("SELECT COUNT(*) AS count, AVG(rating) AS average_rating, SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(`arrived`,`claimed`)))) AS average_delivery_time, SUM(`delivery_fee` + `tip`) AS amount_earned FROM `Orders` WHERE `deliverer`=?");
 $stmt->bind_param("i",$user->id);
 $db->exec();
