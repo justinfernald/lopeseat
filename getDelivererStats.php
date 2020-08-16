18ffced72b498cfbc1e773a0f0f66847e4af8e60
justinfernald <jfernald@my.gcu.edu>
2020-08-16 04:30:21 -0700

    queue work

--- getDelivererStats.php
+++ getDelivererStats.php
@@ -14,6 +14,31 @@ if ($user == null) {
 // amount earned:  SELECT SUM(`delivery_fee` + `tip`) FROM `Orders` WHERE `deliverer`=0
 // combined: SELECT COUNT(*) AS count ,AVG(rating) AS average_rating ,SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(`arrived`,`claimed`)))) AS average_delivery_time, SUM(`delivery_fee` + `tip`) AS amount_earned FROM `Orders` WHERE `deliverer`=0
 
+// fullTimeActive (with session being finished): SELECT endSum - startSum FROM (SELECT
+    // (SELECT SUM(UNIX_TIMESTAMP(time)) FROM DeliveryMode WHERE user_id = 19 AND start = 1) as startSum,
+    // (SELECT SUM(UNIX_TIMESTAMP(time)) FROM DeliveryMode WHERE user_id = 19 AND start = 0) as endSum) as eachSum
+
+// can check if session is done: SELECT start FROM DeliveryMode WHERE user_id=? ORDER BY time DESC LIMIT 1
+
+// get currently active:
+/*
+SELECT
+  DeliveryMode.*
+FROM
+  (SELECT
+     user_id, MAX(time) AS time
+   FROM
+     DeliveryMode
+   GROUP BY
+     user_id) AS latest_change
+INNER JOIN
+  DeliveryMode
+ON
+  DeliveryMode.user_id = latest_change.user_id AND
+  DeliveryMode.time = latest_change.time
+WHERE start = 1
+*/
+
 $db = new db();
 $stmt = $db->prepare("SELECT COUNT(*) AS count, AVG(rating) AS average_rating, SEC_TO_TIME(AVG(TIME_TO_SEC(TIMEDIFF(`arrived`,`claimed`)))) AS average_delivery_time, SUM(`delivery_fee` + `tip`) AS amount_earned FROM `Orders` WHERE `deliverer`=?");
 $stmt->bind_param("i",$user->id);
---
+++ stopDeliveryMode.php
@@ -0,0 +1,37 @@
+<?php
+require('api.php');
+
+$user = $GLOBALS['user'];
+
+if ($user == null || !$user->deliverer) {
+    result(false, "Not logged in.");
+    exit();
+}
+
+$userId = $user->id;
+
+$db = new db();
+
+$stmt = $db->prepare("SELECT start FROM DeliveryMode WHERE user_id=? ORDER BY time DESC LIMIT 1");
+$stmt->bind_param("i",$userId);
+$db->exec();
+
+$row = $db->get()->fetch_object();
+if ($row->start == 0) {
+    result(false, "Not in delivery mode.");
+    exit();
+}
+
+$stmt = $db->prepare("INSERT INTO `DeliveryMode` (`id`, `user_id`, `start`, `time`) VALUES (NULL, ?, 0, NULL)");
+$stmt->bind_param("i",$userId);
+$db->exec();
+
+$stmt = $db->prepare("SELECT id,UNIX_TIMESTAMP(time)*1000 as time FROM DeliveryMode WHERE user_id=? AND start=0 ORDER BY time DESC LIMIT 1");
+$stmt->bind_param("i",$userId);
+$db->exec();
+
+$row = $db->get()->fetch_object();
+
+$message = array("sessionId"=>$row->id,"stoppingTime"=>$row->time);
+result(true, $message);
+?>
