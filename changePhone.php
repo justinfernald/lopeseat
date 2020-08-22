d1425ef9aa5727f00d0940a8fc4da7a025c73fbd
Will Garrett <willgarrett610@gmail.com>
2020-08-22 17:49:17 -0600

    More work on settings

--- changePhone.php
+++ changePhone.php
@@ -5,12 +5,31 @@ use Twilio\Rest\Client;
 
 if (!isLoggedIn()) {
     result(false, "Not logged in");
+    exit();
 }
 
 $db = new db();
 
 $phone = $_POST["phone"];
 $token = randomNum();
+
+if (strlen($phone) < 10) {
+    echo strlen($phone);
+    result(false, "Invalid phone number");
+    exit();
+}
+
+$stmt = $db->prepare("SELECT phone FROM `Users` WHERE phone=? UNION SELECT phone FROM `PhoneConfirmations` WHERE phone=?");
+$stmt->bind_param("ss",$phone,$phone);
+
+$db->exec();
+
+$result = $db->get();
+
+if ($result->num_rows > 0) {
+    result(false, "Phone number is already in use.");
+    exit();
+}
 
 $stmt = $db->prepare("INSERT INTO `PhoneConfirmations` (`user_id`, `phone`, `token`) VALUES (?,?,?)");
 $stmt->bind_param("iss", $GLOBALS['user']->id, $phone, $token);
--- confirm.php
+++ confirm.php
@@ -6,35 +6,26 @@ $token = $_GET["token"];
 
 $db = new db();
 
-$stmt = $db->prepare("SELECT id FROM Users WHERE phone=? AND token=? AND confirmed=0");
-$stmt->bind_param("ss", $phone, $token);
+$stmt = $db->prepare("SELECT `user_id` from PhoneConfirmations WHERE phone=? AND token=?");
+$stmt->bind_param("ss",$phone,$token);
 $db->exec();
-
 $result = $db->get();
 
-$id = 0;
-
-if ($result->num_rows == 0) {
+if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    $id = $row["id"];
+    $user_id = $row['user_id'];
 
-    $stmt = $db->prepare("SELECT * from PhoneConfirmations WHERE phone=? AND token=?");
-    $stmt->bind_param("ss",$phone,$token);
+    $stmt = $db->prepare("UPDATE `Users` SET `confirmed`=1, `phone`=? WHERE `id`=?");
+    $stmt->bind_param("si",$phone,$user_id);
     $db->exec();
-    $result = $db->get();
-
-    if ($result->num_rows == 0) {
-        result(false,"Could not find phone number with that token.");
-        exit(0);
-    } else {
-        $stmt = $db->prepare("DELETE FROM `PhoneConfirmations` WHERE `user_id`=?");
-        $stmt->bind_param("i", $GLOBALS['user']->id);
-        $db->exec();
-    }
+    
+    $stmt = $db->prepare("DELETE FROM `PhoneConfirmations` WHERE `user_id`=?");
+    $stmt->bind_param("i", $user_id);
+    $db->exec();
+    
+    result(true);
+    exit(0);
 }
 
-$stmt = $db->prepare("UPDATE `Users` SET `confirmed`=1, `phone`=? WHERE `id`=?");
-$stmt->bind_param("si",$phone,$id);
-$db->exec();
-result(true);
+result(false,"Could not find phone number with that token.");
 ?>
