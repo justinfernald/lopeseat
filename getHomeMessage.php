20995799e753e1e4454e6ee653c5fee8f9455671
Will Garrett <willgarrett610@gmail.com>
2020-09-20 16:09:13 -0600

    Moved message to server side

---
+++ getHomeMessage.php
@@ -0,0 +1,9 @@
+<?php require('api.php') ?>
+
+<div style="color: rgb(102, 102, 102); font-size: 0.9em; padding: 5px;">
+    Deals and restaurants will be posted here soon. For now, you can start with the search tab below to view the available restaurants. If you have any questions, email us at <span style="font-weight: 600;">lopeseat@lopeseat.com</span>
+</div>
+
+<div style="color: rgb(102, 102, 102); font-size: 0.9em; padding: 5px;">
+    Security is the top priority for us, so we track everytime your barcode is used by a runner. If you suspect any fraudlent use of your dining dollars, let us know and we will be on the case.
+</div>
