8c85d2d6f3c56e18636c32d644aae5f56dadbfb1
justinfernald <jfernald@my.gcu.edu>
2020-11-04 13:30:27 -0700

    fixed category issue

--- menu/getCategory.php
+++ menu/getCategory.php
@@ -1,35 +1,27 @@
 <?php
 require '../api.php';
 
-function utf8ize($d)
-{
-    if (is_array($d)) {
-        foreach ($d as $k => $v) {
-            $d[$k] = utf8ize($v);
-        }
-    } else if (is_object($d)) {
-        foreach ($d as $k => $v) {
-            $d->$k = utf8ize($v);
-        }
-    } else {
-        return utf8_encode($d);
-    }
-
-    return $d;
-}
-
 $rid = $_GET['id'];
 
 $db = new db();
-$stmt = $db->prepare("SELECT id,name,image FROM ItemCategories WHERE restaurant_id=? ORDER BY priority DESC");
+$stmt = $db->prepare("SELECT
+    ItemCategories.id,
+    ItemCategories.name,
+    ItemCategories.image,
+    Restaurants.name AS restaurantName
+FROM
+    ItemCategories
+INNER JOIN Restaurants ON
+    ItemCategories.restaurant_id = Restaurants.id
+WHERE
+    ItemCategories.id = ?");
 $stmt->bind_param("i", $rid);
 $db->exec();
 $results = $db->get();
 
-$categories = [];
+if ($category = $results->fetch_object()) {
+    echo json_encode($category);
+} else {
+    echo json_encode(false);
 
-while ($category = $results->fetch_object()) {
-    array_push($categories, utf8ize($category));
 }
-
-echo json_encode($categories);
